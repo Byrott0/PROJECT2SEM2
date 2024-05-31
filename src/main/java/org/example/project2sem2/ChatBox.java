c2d09803feef453f04a18e7f7c4ce8133044f7b9
Byrott0 <amirsobhan2004@gmail.com>
2024-05-31 10:33:01 +0200

    push l

---
+++ src/main/java/org/example/project2sem2/ChatBox.java
@@ -0,0 +1,8 @@
+package org.example.project2sem2;
+
+import javafx.event.ActionEvent;
+
+public class ChatBox {
+    public void onHelloButtonClick(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/org/example/project2sem2/SignUpController.java
@@ -0,0 +1,8 @@
+package org.example.project2sem2;
+
+import javafx.event.ActionEvent;
+
+public class SignUpController {
+    public void onHelloButtonClick(ActionEvent actionEvent) {
+    }
+}
