c3cf2cbaeaca3d146ea6f33ab103289231244699
Byrott0 <amirsobhan2004@gmail.com>
2024-06-05 17:55:11 +0200

    login scherm 90% af

--- src/main/java/org/example/project2sem2/SignUpController.java
+++ src/main/java/org/example/project2sem2/SignUpController.java
@@ -1,8 +1,26 @@
 package org.example.project2sem2;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class SignUpController {
-    public void onHelloButtonClick(ActionEvent actionEvent) {
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    public void onMeldAanButtonClick(ActionEvent actionEvent) {
+        // Voer hier de actie uit die je wilt doen wanneer de "Meld aan" knop wordt ingedrukt
+    }
+
+    @FXML
+    public void onIkBenAlEenGebruikerButtonClick(ActionEvent actionEvent) {
+        // Voer hier de actie uit die je wilt doen wanneer de "Ik ben al een gebruiker" knop wordt ingedrukt
     }
 }
