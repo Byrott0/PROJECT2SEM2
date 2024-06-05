b34e01f2d81c5450ca3141e8c4239af11a9de78b
Byrott0 <amirsobhan2004@gmail.com>
2024-06-05 23:41:43 +0200

    chatbox  wordt gemaakt

--- src/main/java/org/example/project2sem2/Chat.java
+++ src/main/java/org/example/project2sem2/Chat.java
@@ -1,19 +1,41 @@
 package org.example.project2sem2;
 
-public class Chat {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
+public class Chat {
+    @FXML
+    private Button chatlistID;
+    @FXML
+    private Button imageFileChooserID;
+    @FXML
+    private Button textfieldEnterID;
 
     private String ChatBot;
     private String ResponsiveMessage;
-
-
 
     public Chat(String ResponsiveMessage, String ChatBot) {
         this.ResponsiveMessage = ResponsiveMessage;
         this.ChatBot = ChatBot;
     }
 
-    public void berichtenReader() {
 
+    public void addChat(ActionEvent actionEvent) {
+    }
+
+    public void openFileChooser(ActionEvent actionEvent) {
+    }
+
+    public void checkText(ActionEvent actionEvent) {
+    }
+
+    public void goToAccountBeheerScherm(ActionEvent actionEvent) {
+    }
+
+    public void goToAdminPage(ActionEvent actionEvent) {
+    }
+
+    public void switchToLoginPage(ActionEvent actionEvent) {
     }
 }
--- src/main/java/org/example/project2sem2/GesprekGeschiedenis.java
+++ src/main/java/org/example/project2sem2/GesprekGeschiedenis.java
@@ -1,9 +1,18 @@
 package org.example.project2sem2;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class GesprekGeschiedenis {
+    @FXML
+    private Button chatlistID;
+    @FXML
+    private Button imageFileChooserID;
+    @FXML
+    private Button textfieldEnterID;
+
     public void onHelloButtonClick(ActionEvent actionEvent) {
+        // Je kan nu de knoppen in deze methode gebruiken
     }
-    //enter
 }
