48fa8378912fb82a530d95beb56af8146fe05262
Byrott0 <amirsobhan2004@gmail.com>
2024-06-06 23:05:57 +0200

    small commit niks bijzonder

--- src/main/java/org/example/project2sem2/Admin.java
+++ src/main/java/org/example/project2sem2/Admin.java
@@ -1,10 +1,10 @@
-//package org.example.project2sem2;
-//
-//public class Admin extends Main {
-//
-//    public Admin(String Username, String Email, String Password) {
-//        super();
-//    }
-//
-//
-//}
+package org.example.project2sem2;
+
+public class Admin extends Main {
+
+    public Admin(String Username, String Email, String Password) {
+        super();
+    }
+
+
+}
