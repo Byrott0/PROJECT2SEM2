520fd4d5bf437cf0c5f40fba5f9e17530f6af0d5
kopofall <m.madikh19@gmail.com>
2024-06-06 13:14:35 +0200

    test

--- src/main/java/org/example/project2sem2/Logincontroller.java
+++ src/main/java/org/example/project2sem2/Logincontroller.java
@@ -12,7 +12,7 @@ public class Logincontroller extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Logincontroller.class.getResource("Login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
-        stage.setTitle("Hello!");
+        stage.setTitle("Summarize42 Login");
         stage.setScene(scene);
         stage.show();
     }
