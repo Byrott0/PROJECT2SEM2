c1ba09c92e9c35d7ae6c38cca7ce5b44de87c5ba
samuel2597 <164479057+samuel2597@users.noreply.github.com>
2024-06-07 10:38:04 +0200

    fxml

--- src/main/java/org/example/project2sem2/Login.java
+++ src/main/java/org/example/project2sem2/Login.java
@@ -70,8 +70,31 @@ public class Login {
     }
 
     private boolean isValidCredentials(String username, String password) {
-        DataBaseLogin dataBaseLogin = new DataBaseLogin();
-        return dataBaseLogin.login(username, password);
+        // Maak verbinding met de database
+        String url = "jdbc:sqlite:identifier.sqlite";
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                // Maak een SQL-query om de gebruiker op te zoeken
+                String sql = "SELECT * FROM users WHERE name = ? AND password = ?";
+
+                // Voer de query uit
+                try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+                    pstmt.setString(1, username);
+                    pstmt.setString(2, password);
+                    try (ResultSet rs = pstmt.executeQuery()) {
+                        // Als er een resultaat is, dan is de gebruikersnaam en het wachtwoord geldig
+                        if (rs.next()) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        // Als we hier komen, dan is de gebruikersnaam en het wachtwoord niet geldig
+        return false;
     }
 
     private void LoginNaarChatbox(ActionEvent event) {
