e12f7c98a28a97bb62bf74b0424beb7dfca9b001
vhry72 <102267967+vhry72@users.noreply.github.com>
2024-06-07 22:19:02 +0200

    Hotfix

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,9 +3,6 @@ module org.example.project2sem2 {
     requires javafx.fxml;
     requires java.sql;
 
-
     opens org.example.project2sem2 to javafx.fxml;
     exports org.example.project2sem2;
-    exports Database;
-    opens Database to javafx.fxml;
 }
