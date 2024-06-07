c45cc39c80d9121e9a4dac83edefde95b59fd308
Veysel Yildirim <23067535@student.hhs.nl>
2024-06-07 10:07:14 +0200

    database

--- src/main/java/org/example/project2sem2/DataBaseLogin.java
+++ src/main/java/org/example/project2sem2/DataBaseLogin.java
@@ -1,4 +1,31 @@
 package org.example.project2sem2;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class DataBaseLogin {
+
+    private databaseConnection databaseConnection = new databaseConnection();
+
+    public boolean login(String username, String password) {
+        String selectQuery = "SELECT * FROM credentials WHERE username = ? AND password = ?";
+
+        try (Connection connection = databaseConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(selectQuery)) {
+
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    return true; // User found
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false; // User not found
+    }
 }
