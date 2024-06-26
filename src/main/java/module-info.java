594a8f49c37a2da50e83396e12ce7933983b572e
Byrott0 <amirsobhan2004@gmail.com>
2024-06-26 13:58:21 +0200

    rollback

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,14 @@
 module org.example.project2sem2 {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
+    requires mysql.connector.j;
+    requires org.slf4j;
 
     opens org.example.project2sem2 to javafx.fxml;
     exports org.example.project2sem2;
+    exports org.example.project2sem2.Controller;
+    opens org.example.project2sem2.Controller to javafx.fxml;
+    exports org.example.project2sem2.Utils;
+    opens org.example.project2sem2.Utils to javafx.fxml;
 }
